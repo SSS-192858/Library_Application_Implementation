5a72b02410b7342879dc6a667ae06f0d7091bfbc
Sankalp Kothari <sankalpkothari04@gmail.com>
2023-06-29 21:19:50 +0530

    Update user_services.js

--- libfr_web/src/services/user_services.js
+++ libfr_web/src/services/user_services.js
@@ -79,3 +79,36 @@ export const deleteBook = async(bookCode) => {
     await axios.delete(API_URL + `books/deleteBook/${bookCode}`, { headers: { Authorization: "Bearer " + token } })
     return "Book Successfully Deleted!";
 }
+
+export const updateBook = async(bookCode, bookTitle, bookDesc, author) => {
+    var token = authHeader();
+
+    await axios.put(API_URL + "books/updateBook", {
+        bookCode,
+        bookTitle,
+        author,
+        bookDesc
+    }, { headers: { Authorization: "Bearer " + token } });
+
+    return "Book Data updated successfully";
+}
+
+export const deleteStudent = async(studentId) => {
+
+    await axios.delete(API_URL + `student/deleteStudent/${studentId}`, { headers: { Authorization: "Bearer " + authHeader() } });
+    return "Student Deleted Successfully";
+}
+
+export const updateStudent = async(id, studentName, email, phone) => {
+    var token = authHeader();
+
+    await axios.put(API_URL + "student/updateStudent", {
+        id,
+        studentName,
+        email,
+        phone
+    }, { headers: { Authorization: "Bearer " + token } });
+
+    return "Data saved successfully";
+}
+
